23bb4239a75aed3ecc4f2375dc7af6962dfbc72e
JacoT1989-dotcom <jacothiart52@gmail.com>
2025-03-02 12:01:09 +0200

    set

--- next.config.js
+++ next.config.js
@@ -11,12 +11,6 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     unoptimized: true,
   },
-  env: {
-    SHOPIFY_STORE_DOMAIN: process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN,
-    SHOPIFY_STOREFRONT_ACCESS_TOKEN:
-      process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
-  },
-  // Don't use output: 'export' for the hybrid approach
 
   experimental: {
     missingSuspenseWithCSRBailout: false,
